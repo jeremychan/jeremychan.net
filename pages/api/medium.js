7b4f8c2a4e6fa3cc63efd8337d6ff4123affe99a
jeremychan <chanchunwing@gmail.com>
2021-02-21 18:35:45 +0000

    convert rss feed to json

--- pages/api/medium.js
+++ pages/api/medium.js
@@ -1,12 +1,13 @@
 const axios = require("axios"); 
+const xml2js = require("xml2js"); 
 
 export default async (req, res) => {
     const response = await axios
         .get('https://medium.com/feed/@jeremy-chan')
-        .then(({ data }) => {
-            return data
-        })
+        .then(async (res) => {
+            return await xml2js.parseStringPromise(res.data, (_err, result) => result);
+        });
     res.send({
-        text: response,
+        ...response,
     })
 }
